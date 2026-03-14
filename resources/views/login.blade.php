6c782a2dd6ac12f27a568c88e088242f6f775973
vikramalhakim1-art <vikramalhakim1@gmail.com>
2026-03-15 01:12:15 +0700

    P
    P

---
+++ resources/views/login.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+<h1>Halaman Login</h1>
+
+<form>
+    <label>Email</label><br>
+    <input type="text"><br><br>
+
+    <label>Password</label><br>
+    <input type="password"><br><br>
+
+    <button>Login</button>
+</form>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ItemController;
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/items', [ItemController::class, 'index']);
